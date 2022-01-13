4150bd2be9b0376ef4a0541a215accf19d1f85d5
james <james@quizlicious.com>
2022-01-12 22:12:43 -0500

    Added user list for admins

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -39,9 +39,56 @@ function UserListScreen() {
         dispatch(listUsers())
     }, [dispatch])
 
+    const deleteHandler = (id) => {
+        console.log('Delete user id', id)
+
+    }
+
     return (
         <div>
             <h1>Users</h1>
+            { loading 
+                ? (<Loader/>)
+                : error 
+                    ? (<Message variant='danger'>{error}</Message> )
+                    : (
+                        <Table striped bordered hover responsive className='table-sm'>
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>NAME</th>
+                                    <th>EMAIL</th>
+                                    <th>ADMIN</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {users.map(user => (
+                                    <tr key={user._id}>
+                                        <td>{user._id}</td>
+                                        <td>{user.name}</td>
+                                        <td>{user.email}</td>
+                                        <td>{user.isAdmin ? (
+                                            <i className='fas fa-check' style={{color:'green'}}/>
+                                        ) : (
+                                            <i className='fas fa-times' style={{color:'red'}}></i>
+                                        )}</td>
+                                        <td>
+                                            <LinkContainer to={`/admin/user/${user.id}`}>
+                                                <Button variant='light' className='btn-sm'>
+                                                    <i className='fas fa-edit'></i>
+                                                </Button>
+                                            </LinkContainer>
+                                            <Button variant='danger' className='btn-sm' onClick={() => deleteHandler(user._id)}>
+                                                <i className='fas fa-trash'></i>
+                                            </Button>
+
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </Table>
+                    )}
         </div>
     )
 }
