ac7fec351916c139a1cbfc329b38574864cc9929
james <james@quizlicious.com>
2022-01-13 16:19:13 -0500

    Register UserEditScreen in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
 
 function App() {
   return (
@@ -31,7 +32,8 @@ function App() {
           <Route path='/placeorder' component={PlaceOrderScreen}/>
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
-          <Route path='/admin/userList' component={UserListScreen}/>
+          <Route path='/admin/userlist' component={UserListScreen}/>
+          <Route path='/admin/user/:id/edit/' component={UserEditScreen}/>
         </Container>
       </main>
       <Footer />
