33b45c5b927cbf4c315d49aa8db6d34f811b0803
james <james@quizlicious.com>
2022-01-13 14:09:23 -0500

    Delete users in frontend admin screen

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -4,7 +4,7 @@ import { Table, Button } from 'react-bootstrap'
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import { useDispatch, useSelector} from 'react-redux'
-import { listUsers } from '../actions/userActions';
+import { listUsers, deleteUser } from '../actions/userActions';
 
 import { 
     USER_LOGIN_REQUEST,
@@ -38,17 +38,21 @@ function UserListScreen({history}) {
     const userLogin = useSelector(state => state.userLogin)
     const { userInfo } = userLogin
 
+    const userDelete = useSelector(state => state.userDelete)
+    const { success:successDelete } = userDelete
+
     useEffect(() => {
         if(userInfo && userInfo.isAdmin) {
             dispatch(listUsers())
         } else {
             history.push('/login')
         }
-    }, [dispatch, history])
+    }, [dispatch, history, successDelete])
 
     const deleteHandler = (id) => {
-        console.log('Delete user id', id)
-
+        if(window.confirm('Are you sure you want to delete this user?')) {
+            dispatch(deleteUser(id))
+        }
     }
 
     return (
